8bc6003d1ba7ea7ca3d863458364bf7e56a8c196
Francisco Santana Cardoso <fsantanagy@gmail.com>
2024-01-07 16:37:13 -0300

    adicionado Post, put

--- Aula_56_CRUD/index.js
+++ Aula_56_CRUD/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 
 const server = express();
+const cursos = ["JavaScript", "Node JS", "Python", "React"]
+
+//indicando que posso receber um json
+server.use(express.json());
 
 server.get('/cursos', (req, res)=>{
+    return res.json(cursos);
+});
+
+server.get('/cursos/:id', (req, res) => {
+    const id = req.params.id;
+    return res.json(cursos[id]);
+});
+
+// POST - Adicionando curso
+server.post('/cursos', (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+// PUT - Editando curso
+server.put('/cursos/:id', (req, res)=> {
+    const { id }  = req.params
+    const { name } = req.body
+
+    cursos[id] = name
+
+    return res.json(cursos)
 
 });
+
 
 server.listen(2000)
