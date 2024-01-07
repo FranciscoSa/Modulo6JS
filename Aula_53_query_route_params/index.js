d11486d37547d5082c9477dbb5e91742c8917535
Francisco Santana Cardoso <fsantanagy@gmail.com>
2024-01-06 21:50:31 -0300

    exemplo de Route params

--- Aula_53_query_route_params/index.js
+++ Aula_53_query_route_params/index.js
@@ -6,9 +6,14 @@ const server = express();
 //Route params =  /curso/2
 //Request body = { node: 'Nodejs', tipo: 'Backend'}
 
-server.get('/cursos', (req, res) => {
-    const nome = req.query.nome;
-    return res.json({nome: `Aprendendo ${nome}`})
+server.get('/cursos/:id', (req, res) => {
+    const nome = req.query.nome; //Query params
+    const id = req.params.id; //Route params
+
+    return res.json({
+                        nome: `Aprendendo ${nome}`,
+                        id: id
+                    })
 });
 
 server.listen(2000);
