5a024f85611d9c125ea3a84c90eaba5f44eaf478
Francisco Santana Cardoso <fsantanagy@gmail.com>
2024-01-08 20:11:42 -0300

    adicionado o middlewares por escopo

--- Aula_57_middlewares/index.js
+++ Aula_57_middlewares/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const server = express();
+const cursos = ["Java Script", "React"];
+
+server.use(express.json())
 
 //middleware Global
 server.use((req, res, next)=>{
@@ -8,8 +11,26 @@ server.use((req, res, next)=>{
     return next();
 });
 
+//middleware local
+function checkCursoName(req, res, next){
+    if (!req.body.name || req.body.name === null){
+        return res.status(400).json({ error: "O campo nome do curso é obrigatorio" });
+    }
+
+    return next();
+}
+
+
 server.get("/cursos", (req, res) => {
-    return res.json({casa: "bola"});
+    return res.json(cursos);
+});
+
+//usando middleware local
+server.post("/cursos", checkCursoName, (req, res)=> {
+    const { name } = req.body;
+    cursos.push(name);
+    
+    return res.json(cursos);
 });
 
 server.listen(2000);
